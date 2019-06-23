82d7328acde03f76d6b285fbb4b8023a4b79a7fc
Sandy Masia <macxermillio@gmail.com>
2019-06-23 13:34:38 +0200

    "Edits on the about page"

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('#menu');
 
 const sideNav = document.querySelector('#sideNav');
 
+
 menu.addEventListener('click', event => {
   if (sideNav.style.width == '7em' ) {
     return sideNav.style.width = '25em';
@@ -10,5 +11,29 @@ menu.addEventListener('click', event => {
   }
 })
 
+const menu_mb = document.querySelector('#col-menu');
+const lists = document.querySelector('.mb-navbar')
+menu_mb.addEventListener('click' , ()=>{
+  const ths = document.querySelector('#col-menu');
+  lists.classList.toggle("nav-open");
+  ths.classList.toggle('fixed')
 
+})
+
+const settings = document.querySelector('.settings');
+
+const settingsPanel = document.querySelector('#settingsPanel')
+
+settings.addEventListener('click', event => {
+  if (settingsPanel.style.display == 'none'  ) {
+
+    settingsPanel.style.display = 'flex';
+    
+  } else {
+
+    settingsPanel.style.display = 'none';
+
+  }
+}
+  )
 
